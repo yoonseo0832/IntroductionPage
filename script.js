4e3c7b8dd213e8450ef8012a110124e544121f35
Yoonseo <145835135+yoonseo0832@users.noreply.github.com>
2024-10-31 10:18:18 +0900

    수정해봄
    .onclik으로 navitem을 클릭시 화이트로 글자가 바뀌고 해당 페이지 이동하도록

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
- var main = document.querySelector('#main');
-main.onclick = function(){
-    main.style.color = "black"
-}
+const navItems = document.querySelectorAll('.nav-item');
+navItems.forEach(item => {
+    item.addEventListener('click', function(event){
+        navItems.forEach(nav => {
+            nav.style.color='';
+        });
+        this.style.color='white';
+        const targetSectionId = this.getAttribute('href');
+        DocumentTimeline.querySelector(targetSectionId).scrollIntoView({
+            behavior: 'smooth'
+        });
+        event.preventDefault();
+    });
+});
