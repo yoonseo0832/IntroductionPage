78ebd7d9f1c3d13211f6c0a29a37b7b45d240ac8
Yoonseo <145835135+yoonseo0832@users.noreply.github.com>
2024-11-06 17:37:52 +0900

    수정

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function(){ //브라우저에서 H
             }
         });
         navLinks.forEach((link) => {
+            link.classList.remove("active");
             link.style.color = "grey";
         });
         if(currentSection){
